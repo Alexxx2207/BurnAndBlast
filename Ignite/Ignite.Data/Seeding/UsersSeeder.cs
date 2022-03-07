a5934ac5bb55464cea11ee397bf3f218d12301ed
Alexxx2207 <alex.ivanov0722@gmail.com>
2022-03-07 03:28:33 +0200

    Added Login UI.

---
+++ Ignite/Ignite.Data/Seeding/UsersSeeder.cs
@@ -0,0 +1,50 @@
+﻿using Ignite.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignite.Data.Seeding
+{
+    public class UsersSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            await SeedUserAsync(userManager, "as@as.as", "as@as.as", "Mario", "Avramov", "123456", role: GlobalConstants.GlobalConstants.AdministratorRoleName);
+        }
+
+        private static async Task SeedUserAsync(UserManager<ApplicationUser> userManager, 
+            string email = null, string userName = null, string firstName = null, string lastName = null, string password = null,
+            string profilePicture = null, string role = null)
+        {
+            var userExists = await userManager.FindByNameAsync(userName);
+            if (userExists == null)
+            {
+                var user = new ApplicationUser()
+                { 
+                    Id = Guid.NewGuid().ToString(),
+                    Email = email,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    ProfilePicture = profilePicture,
+                    UserName = userName,
+                };
+
+                var result = await userManager.CreateAsync(user, password);
+                if (!result.Succeeded)
+                {
+                    throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+                }
+                else
+                {
+                    await userManager.AddToRoleAsync(user, role);
+                }
+            }
+        }
+    }
+}
