6abf89a131d8c1edc4150f50b2a287ea971da42b
Alexxx2207 <alex.ivanov0722@gmail.com>
2022-03-15 11:27:59 +0200

    Modified Profile Page. Enable adding, changing and removing profile picture.

---
+++ Ignite/Ignite.Services/Users/IUsersService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignite.Services.Users
+{
+    public interface IUsersService
+    {
+        Task SaveImage(IFormFile file);
+
+        void RemoveImage(string userId);
+    }
+}
---
+++ Ignite/Ignite.Services/Users/UsersService.cs
@@ -0,0 +1,42 @@
+﻿using Ignite.Data;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignite.Services.Users
+{
+    public class UsersService : IUsersService
+    {
+        private readonly ApplicationDbContext db;
+
+        public UsersService(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void RemoveImage(string userId)
+        {
+            var user = db.Users.Find(userId);
+
+            File.Delete($"wwwroot/Profile Pics/{user.ProfilePicture}");
+
+            user.ProfilePicture = null;
+            db.SaveChanges();
+        }
+
+        public async Task SaveImage(IFormFile file)
+        {
+            if (file.Length > 0)
+            {
+                using (var stream = new FileStream("~/Profile Pics", FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+
+                }
+            }
+        }
+    }
+}
